3a9f134144e277dbc179f1fd39a330efc32e20d8
Ollie Cvetkovic <olliecvetkovic@Ollies-MacBook-Air-2.local>
2024-10-13 04:37:35 +0100

    working database

--- contracts/soroban_dict/contracts/increment/src/lib.rs
+++ contracts/soroban_dict/contracts/increment/src/lib.rs
@@ -1,24 +1,22 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, log, symbol_short, Env, Symbol, Val, Map};
+use soroban_sdk::{contract, contractimpl, contracttype, log, symbol_short, token, Address, Env, Map, Symbol, Val, Vec};
 
 const DATA_KEY: Symbol = symbol_short!("DATA"); // Unique key for your map
 
-const COUNTER: Symbol = symbol_short!("COUNTER");
-
 #[contract]
-pub struct IncrementorContract;
+pub struct CoursesContract;
 
 // Define the state of the contract
 #[contracttype]
 pub struct MyState {
-    pub data: Map<Symbol, Val>, // Map with Symbol keys and i32 values
+    pub data: Map<Address, Vec<Val>>, // Map with Symbol keys and i32 values
 }
 
 impl MyState {
     // Constructor for MyState
     pub fn new(env: &Env) -> Self {
         // Check if the map already exists in storage
-        if let Some(existing_map) = env.storage().persistent().get::<_, Map<Symbol, Val>>(&DATA_KEY) {
+        if let Some(existing_map) = env.storage().persistent().get::<_, Map<Address, Vec<Val>>>(&DATA_KEY) {
             // If it exists, return the existing state
             Self {
                 data: existing_map,
@@ -36,30 +34,77 @@ impl MyState {
 
 
 #[contractimpl]
-impl IncrementorContract {
-    /// Increment an internal counter; return the new value.
-    pub fn increment(env: Env, user_id: Symbol, course_id: Val) -> u32 {
-
+impl CoursesContract {
+    /// Adding Value to Database
+    pub fn append_database(env: Env, user_id: Address, course_id: Val) {
         let mut state = MyState::new(&env);
 
-        // Insert (user_id, course_id) into the map
-        state.data.set(user_id.clone(), course_id);
+        if let Some(mut courses) = state.data.get(user_id.clone()) {
+            courses.push_back(course_id.clone());
+            state.data.set(user_id.clone(), courses);
+        } else {
+            let mut new_courses = Vec::new(&env);
+            new_courses.push_back(course_id.clone());
+            state.data.set(user_id.clone(), new_courses);
+        }
 
-        log!(&env, "Inserted user_id: {:?} with course_id: {:?}", user_id, course_id);
-
-        // Optionally, you can log the insertion for debugging
-        let mut count: u32 = env.storage().instance().get(&COUNTER).unwrap_or(0);
-
-        count += 1;
-
-        log!(&env, "count: {}", count);
-
-        env.storage().instance().set(&COUNTER, &count);
-
-        env.storage().instance().extend_ttl(100, 100);
-
-        count
+        env.storage().persistent().set(&Symbol::short("DATA"), &state.data);
     }
+
+    /// Retrieve course_ids for a given user_id
+    pub fn retrieve_courses(env: Env, user_id: Address) -> Vec<Val> {
+        let state = MyState::new(&env);
+
+        // Check if the user_id exists in the map
+        if let Some(courses) = state.data.get(user_id) {
+            // User exists, return their course_ids
+            courses
+        } else {
+            // User doesn't exist, return an empty vector
+            Vec::new(&env)
+        }
+    }
+    
+    pub fn purchase(env: Env, buyer: Address, course_id: Val, price: i128, token:Address) {
+        // let owner: Address = env.storage().persistent().get(&Symbol::short("owner"))
+        //     .ok_or_else(|| Error::from_status(Status::InternalError))?;
+
+        let owner: Address = env.current_contract_address();
+        log!(&env, "Owner: {}", owner);
+
+        let token = token::Client::new(&env, &token);
+        token.transfer(&buyer, &owner, &price);
+
+        Self::append_database(env, buyer, course_id);
+
+        fn move_token(
+            env: &Env,
+            token: &Address,
+            from: &Address,
+            to: &Address,
+            max_spend_amount: i128,
+            transfer_amount: i128,
+        ) {
+            let token = token::Client::new(env, token);
+            let contract_address = env.current_contract_address();
+            // This call needs to be authorized by `from` address. It transfers the
+            // maximum spend amount to the swap contract's address in order to decouple
+            // the signature from `to` address (so that parties don't need to know each
+            // other).
+            token.transfer(from, &contract_address, &max_spend_amount);
+            // Transfer the necessary amount to `to`.
+            token.transfer(&contract_address, to, &transfer_amount);
+            // Refund the remaining balance to `from`.
+            token.transfer(
+                &contract_address,
+                from,
+                &(max_spend_amount - transfer_amount),
+            );
+        }
+    }
+
 }
+
+
 
 mod test;
--- contracts/soroban_dict/contracts/increment/src/test.rs
+++ contracts/soroban_dict/contracts/increment/src/test.rs
@@ -1,17 +1,37 @@
 #![cfg(test)]
 
-use crate::{IncrementorContract, IncrementorContractClient};
-use soroban_sdk::{Env, Symbol, Val};
+use crate::{CoursesContract, CoursesContractClient};
+use soroban_sdk::{token, Address, Env, String, Val, Vec};
 
 #[test]
 fn increment() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, IncrementorContract);
-    let client = IncrementorContractClient::new(&env, &contract_id);
-    let course_id = Val::from(1);
-    let user_id: Symbol = Symbol::new(&env, "soy_luke");
+    let contract_id = env.register_contract(None, CoursesContract);
+    let client = CoursesContractClient::new(&env, &contract_id);
+    let course_id1 = Val::from(1);
+    let course_id2 = Val::from(2);
+    let course_id3 = Val::from(3);
+    let user_id: Address = Address::from_string(&String::from_str(&env, "GCTWJNUIM76ZEYDC5VQI4T3X6SDQIZQ7MZHBSMX5ASNGNT7YNJ5YCZEX"));
 
-    assert_eq!(client.increment(&user_id, &course_id), 1);
-    assert_eq!(client.increment(&user_id, &course_id), 2);
-    assert_eq!(client.increment(&user_id, &course_id), 3);
+    // Append course_ids to the user
+    client.append_database(&user_id, &course_id1);
+    client.append_database(&user_id, &course_id2);
+    client.append_database(&user_id, &course_id3);
+
+    // Retrieve the courses for the user and verify the results
+    let retrieved_courses: Vec<Val> = client.retrieve_courses(&user_id);
+
+    // Create the expected vector
+    let mut expected_courses = Vec::new(&env);
+    expected_courses.push_back(course_id1);
+    expected_courses.push_back(course_id2);
+    expected_courses.push_back(course_id3);
+
+    // Assert the retrieved courses match the expected courses
+    assert_eq!(retrieved_courses, expected_courses);
+
+    let token = token::Client::new(&env, &Address::from_string(&String::from_str(&env, "CBHBD77PWZ3AXPQVYVDBHDKEMVNOR26UZUZHWCB6QC7J5SETQPRUQAS4")));
+
+    client.purchase(&user_id, &course_id1, &100i128, &token.address);
+
 }
